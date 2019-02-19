801fcd9901c49e1832e08ec162072beaa4a67c1e
Mika Viinamäki <mika.viinamaki@fraktio.fi>
2019-02-19 23:44:36 +0200

    Support for syncstart

--- BGAnimations/ScreenGameplay underlay/default.lua
+++ BGAnimations/ScreenGameplay underlay/default.lua
@@ -40,4 +40,6 @@ if GAMESTATE:IsPlayerEnabled(PLAYER_1) and GAMESTATE:IsPlayerEnabled(PLAYER_2) t
 	t[#t+1] = LoadActor("./Shared/WhoIsCurrentlyWinning.lua")
 end
 
+t[#t+1] = LoadActor("./Shared/SyncStartScores.lua")
+
 return t
