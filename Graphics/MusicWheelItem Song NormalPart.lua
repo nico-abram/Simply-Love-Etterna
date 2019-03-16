c96834575cc58cbae7cddc4e64d61378319936a8
Nicolas <abramlujan@gmail.com>
2019-03-16 05:28:18 -0300

    Add song wheel item clicking in etterna

--- Graphics/MusicWheelItem Song NormalPart.lua
+++ Graphics/MusicWheelItem Song NormalPart.lua
@@ -1,16 +1,52 @@
-return Def.ActorFrame{
-	InitCommand=function(self)
+local polled = false
+return Def.ActorFrame {
+	InitCommand = function(self)
 		self:x(26)
+		if SL.IsEtterna then
+			self:SetUpdateFunction(
+				function()
+					polled = false
+				end
+			):SetUpdateFunctionInterval(0.05)
+		end
 	end,
-
-	Def.Quad{
-		InitCommand=function(self) self:diffuse(color("#000a11")):zoomto(_screen.w/2.1675, _screen.h/15):diffusealpha(0.5) end
+	Def.Quad {
+		BeginCommand = function(self)
+			self:diffuse(color("#000a11")):zoomto(_screen.w / 2.1675, _screen.h / 15):diffusealpha(0.5)
+			self.top = SCREENMAN:GetTopScreen()
+			self.wheel = self.top:GetMusicWheel()
+		end,
+		LeftClickMessageCommand = SL.IsEtterna and
+			function(self)
+				local mouse = getMousePosition()
+				local mx, my = mouse.x, mouse.y
+				if polled then
+					return
+				end
+				local x, y, w, h =
+					self:GetTrueX(),
+					self:GetTrueY(),
+					self:GetWidth() * self:GetZoomX(),
+					self:GetHeight() * self:GetZoomY()
+				local b = mx > x - w / 2 and my > y - h / 2 and mx < x + w / 2 and my < y + h / 2
+				if b then
+					polled = true
+					local n = math.floor(my / h) - 7
+					SCREENMAN:SystemMessage(tostring(n))
+					local doot = self.wheel:MoveAndCheckType(n)
+					self.wheel:Move(0)
+					if n == 0 or doot == "WheelItemDataType_Section" then
+						self.top:SelectCurrent(0)
+					end
+				end
+			end or
+			nil
 	},
-	Def.Quad{
-		InitCommand=function(self)
-			self:diffuse( ThemePrefs.Get("RainbowMode") and Color.White or color("#0a141b"))
-				:diffusealpha( ThemePrefs.Get("RainbowMode") and 0.5 or 1 )
-				:zoomto(_screen.w/2.1675, _screen.h/15 - 1)
-		 end
+	Def.Quad {
+		InitCommand = function(self)
+			self:diffuse(ThemePrefs.Get("RainbowMode") and Color.White or color("#0a141b")):diffusealpha(
+				ThemePrefs.Get("RainbowMode") and 0.5 or 1
+			):zoomto(_screen.w / 2.1675, _screen.h / 15 - 1)
+		end
 	}
 }
